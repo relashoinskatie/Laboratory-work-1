9bcbba6687ac7c2d3317789cd111be01033d9e31
relashoinskatie <nurlygayanova2005@gmail.com>
2023-10-30 02:09:33 +0300

    Create laba1.py

---
+++ laba1.py
@@ -0,0 +1,33 @@
+sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
+sides = sorted(sides, reverse=True)
+smax = 0
+for i in range(len(sides)):
+    for j in range(i + 1, len(sides)):
+        for k in range(j + 1, len(sides)):
+            a = sides[i]
+            b = sides[j]
+            c = sides[k]
+            if a + b > c and a + c > b and b + c > a:
+                p = (a + b + c) / 2
+                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
+                if s > smax:
+                    smax = s
+print("Максимальная площадь треугольника", smax)
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+D = b**2 - 4 * a * c
+
+if D < 0:
+    x1 = "нет корней"
+    x2 = ""
+if D == 0:
+    x1 = str(-b/2*a) + " один корень у уравнеия"
+    x2 = ""
+else:
+    x1 = str((-b+d**0.5)/2*a) + " первый корень уравнения"
+    x2 = str((-b-d**0.5)/2*a) + " второй корень уравнения"
+
+print(x1, x2)
